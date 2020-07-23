0dc471cdc2e8fb5b5c8f60e82273c1385789a006
ZGennadiy <gennadiy.zinchenko@gmail.com>
2020-07-23 23:40:34 +0300

    make footer

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,21 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', function () {
-  var burger = function burger(menuSelector) {
-    var menuElems = document.querySelectorAll(menuSelector);
-    menuElems.forEach(function (menu) {
-      if (window.screen.availWidth >= 992) {
-        menu.classList.remove('menu_vertical');
-      } else {
-        menu.classList.add('menu_vertical');
-      }
-
-      window.addEventListener('resize', function () {
-        if (window.screen.availWidth >= 992) {
-          menu.classList.remove('menu_vertical');
-        } else {
-          menu.classList.add('menu_vertical');
-        }
-      });
-    });
-  };
-
-  burger('.header__wrapper .menu');
+window.addEventListener('DOMContentLoaded', function () {// const burger = (menuSelector) => {
+  //     const menuElems = document.querySelectorAll(menuSelector);
+  //     menuElems.forEach((menu) => {
+  //         if (window.screen.availWidth >= 992) {
+  //             menu.classList.remove('menu_vertical');
+  //         } else {
+  //             menu.classList.add('menu_vertical');
+  //         }
+  //         window.addEventListener('resize', () => {
+  //             if (window.screen.availWidth >= 992) {
+  //                 menu.classList.remove('menu_vertical');
+  //             } else {
+  //                 menu.classList.add('menu_vertical');
+  //             }
+  //         });
+  //     });
+  // };
+  // burger('.header__wrapper .menu');
 });
